6ca03a4dc04a988861e2e8908d47131ac61acefd
Maximilien Rigaut <max.rigaut@gmail.com>
2018-05-06 17:37:55 +0200

    Before TTS
    Just before TTS, with shotcalling working as it seems

--- mechanikos.py
+++ mechanikos.py
@@ -11,10 +11,20 @@ class Config():
     def __init__(self):
         self.delay=False
         self.miliseconds=False
-        self.font="big"
-        self.color="white"
-        self.on_color=None
+        self.font="fixedsys"
+        self.color="red"
         self.show=1
+        self.offset=5
+        self.size=32
+
+    def valtype(self):
+        self.show=int(self.show)
+        self.offset=int(self.offset)
+        self.size=int(self.size)
+
+    def __str__(self):
+        return "delay:{} mili:{} font:{} color:{} show:{} offset:{} size:{}".format(self.delay,
+        self.miliseconds,self.font,self.color,self.show,self.offset,self.size)
 
 class Timer():
     def __init__(self):
@@ -22,16 +32,69 @@ class Timer():
 
     def __call__(self):
         return time.time()-self.first_time
+
+class shotcaller(threading.Thread):
+    def run(self):
+        pass
+    def join(self):
+        pass
+
+class Timetable():
+    "All the logic, updating and shotcalling"
+    def __init__(self,timetables,config):
+        self.timetables=timetables
+        self.config=config
+        self.waitfor=self.timetables[0][0]
+        self.step=0
+        self.ln=len(self.timetables)
+        self.config=config
+        self.shotcall=0
+
+    def __call__(self,time,updateobject):
+        if self.waitfor<time: # Time to change the text for the next mechanic
+            updateobject.changeText(self.getnext()[1])
+            self.step+=1
+            self.waitfor=self.getnext()[0]
+            self.shotcall=0 # You can shotcall again
+            if self.step>self.ln: #Stop the program
+                updateobject.destroy()
+                sys.exit()
+
+        elif self.waitfor<time+self.config.offset: #Time to shotcall
+            if self.shotcall==0:
+                self.shotcall=1 # You can't shotcall anymore
+                print "Shotcalling"
+                # Only shotcall the lines that exist
+                print self.getnext()
+        else:
+            return None
+
+    def getindex(self,index):
+        try:
+            return self.timetables[index]
+        except:
+            return [self.timetables[self.ln-1][0]+self.config.offset,"",""]
+
+    def getnext(self):
+        return self.getindex(self.step)
+
+    def getnnext(self,nb):
+        r=[]
+        for i in range(nb):
+            r.append(self.getindex(self.step+i))
+        return r
+
 #Utilities
 def checkfile(f):
-    'Check every line of the file for syntax errors'
+    'Check every line of the file for syntax errors and return the config, timetables'
     conf=f[0].split(";")
+    timecheck=0
+    table=[]
     try:
         config=load_conf(f[0])
     except:
         print('Error reading config')
 
-    timecheck=0
     for i in range(1,len(f)):
         try:
             time,show,say=split_line(f[i])
@@ -39,11 +102,11 @@ def checkfile(f):
                 print("Error on the timetables line {}".format(i))
                 raise AssertionError
             timecheck=time
+            table.append([time,show,say])
         except:
             print("error line {} : {}".format(i,f[i]))
             sys.exit()
-
-    return conf
+    return config,Timetable(table,config)
 
 def load_conf(conf):
     "Load the confing in a line"
@@ -56,6 +119,7 @@ def load_conf(conf):
         else:
             var,val=line.split('=')
             setattr(C,var,val)
+    C.valtype()
     return C
 
 def split_line(line):
@@ -81,47 +145,17 @@ try:
         time_data=f.read().splitlines()
     f.close()
     print("Cheking Time Data...")
-    checkfile(time_data)
+    config,timetables=checkfile(time_data)
 except:
     print "Error reading file"
     sys.exit()
 
 
-class shotcaller(threading.Thread):
-    def run(self):
-        root = tk.Tk()
-        root.overrideredirect(True)
-        root.geometry("+100+120")
-        root.lift()
-        root.wm_attributes("-topmost", True)
-        root.wm_attributes("-disabled", True)
-        root.wm_attributes("-transparentcolor", "white")
-        T = tk.Text(root, height=2, width=30,bd=0,fg="red",font=("fixedsys", 22))
-        T.pack()
-        T.insert(tk.END, "Just a text Widget\nin two lines\n")
-
-        root.mainloop()
-
-    def join(self):
-        self.root.quit()
-
-class Timetable():
-    def __init__(self,timetables):
-        self.timetables=timetables
-        self.waitfor=self.timetables[0][0]
-        self.step=0
-
-    def __call__(self,time):
-        if self.waitfor<time:
-            self.step+=1
-            self.waitfor=self.timetables[self.step][0]
-            return self.timetables[self.step-1]
-        else:
-            return None
-
 class Application(tk.Frame):
-    def __init__(self, master=None,gtimer=Timer(),shotcaller=None,timetables=None):
+    def __init__(self, master=None,gtimer=Timer(),config=None,shotcaller=None,timetables=None):
         self.timer=gtimer
+        self.timetables=timetables
+        self.gconfig=config
         tk.Frame.__init__(self, master)
         master.overrideredirect(True)
         master.geometry("+100+120")
@@ -143,10 +177,13 @@ class Application(tk.Frame):
 
     def onUpdate(self):
         # update displayed time
-        self.now.set(self.timer())
+        self.timetables(self.timer(),self)
         # schedule timer to call myself after 1 second
         self.after(100, self.onUpdate)
 
+    def changeText(self,newtext):
+        self.now.set(newtext)
+
 root = tk.Tk()
-app = Application(master=root,gtimer=gtimer,shotcaller=None,timetables=None)
+app = Application(master=root,gtimer=gtimer,config=config,shotcaller=None,timetables=timetables)
 root.mainloop()
